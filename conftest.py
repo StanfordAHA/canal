7859c882ecd6b02717293fead461093e9a25543a
Raj Setaluri <raj.setaluri@gmail.com>
2019-02-22 11:31:50 -0800

    Add resetting logic in conftest

---
+++ conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from magma import clear_cachedFunctions
+import magma.backend.coreir_ as coreir_
+
+@pytest.fixture(autouse=True)
+def magma_test():
+    clear_cachedFunctions()
+    coreir_.__reset_context()
