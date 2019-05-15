8a754ba359fc7d4029cf5b0928c0ce9284c98851
Keyi Zhang <keyi@stanford.edu>
2019-05-14 20:15:16 -0700

    fix sb empty core

--- canal/circuit.py
+++ canal/circuit.py
@@ -379,8 +379,9 @@ class TileCircuit(generator.Generator):
                     assert bit_width == port_node.width
 
             # switch box time
+            core_name = self.core.name() if self.core is not None else ""
             sb = SB(tile.switchbox, config_addr_width, config_data_width,
-                    self.core.name())
+                    core_name)
             self.sbs[sb.switchbox.width] = sb
 
         # lift all the sb ports up
