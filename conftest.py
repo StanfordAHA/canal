67fcf38c7a5ae81c12551dfca831def6bdf9f63a
Keyi Zhang <keyi@stanford.edu>
2019-05-16 16:08:51 -0700

    hotfix: add clear gemstone cacme

--- conftest.py
+++ conftest.py
@@ -1,9 +1,11 @@
 import pytest
 from magma import clear_cachedFunctions
 import magma.backend.coreir_ as coreir_
+from gemstone.generator import clear_generator_cache
 
 
 @pytest.fixture(autouse=True)
 def magma_test():
     clear_cachedFunctions()
     coreir_.__reset_context()
+    clear_generator_cache()
