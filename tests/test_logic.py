a7cb7e07c97d4ec3d617df81bd97e2f81ffa4262
Keyi Zhang <keyi@stanford.edu>
2022-08-05 16:02:19 -0700

    add split fifo logic; need to finish the intergration

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -22,11 +22,6 @@ def test_reg_fifo():
     tester.poke(circuit.I, 42)
     tester.eval()
     tester.step(2)
-    tester.expect(circuit.ready_out, 1)
-    tester.poke(circuit.valid_in, 1)
-    tester.poke(circuit.I, 43)
-    tester.eval()
-    tester.step(2)
     # should be full
     tester.expect(circuit.ready_out, 0)
     # pop out data
@@ -35,8 +30,33 @@ def test_reg_fifo():
     tester.poke(circuit.valid_in, 0)
     tester.poke(circuit.ready_in, 1)
     tester.step(2)
-    tester.expect(circuit.valid_out, 1)
-    tester.expect(circuit.O, 43)
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        tester.compile_and_run(target="verilator",
+                               magma_output="coreir-verilog",
+                               directory=tempdir,
+                               flags=["-Wno-fatal"])
+
+
+def test_reg_fifo_sr():
+    fifo = FifoRegWrapper(16)
+    circuit = fifo.circuit()
+    tester = BasicTester(circuit,
+                         circuit.clk,
+                         circuit.ASYNCRESET)
+
+    tester.zero_inputs()
+    tester.reset()
+    tester.poke(circuit.CE, 1)
+    # enable fifo mode
+    tester.poke(circuit.fifo_en, 0)
+
+    for i in range(10):
+        tester.poke(circuit.I, i + 1)
+        tester.eval()
+        if i > 1:
+            tester.expect(circuit.O, i + 1 - 1)
+        tester.step(2)
 
     with tempfile.TemporaryDirectory() as tempdir:
         tester.compile_and_run(target="verilator",
@@ -46,4 +66,4 @@ def test_reg_fifo():
 
 
 if __name__ == "__main__":
-    test_reg_fifo()
+    test_reg_fifo_sr()
